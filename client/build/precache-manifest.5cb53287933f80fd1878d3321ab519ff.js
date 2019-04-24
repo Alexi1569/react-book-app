c05e0616100b02ab048a2617ebaca148a850911d
Alexi1569 <farikprof@Gmail.com>
2019-04-24 15:12:22 +0300

    deploy to heroku

---
+++ client/build/precache-manifest.5cb53287933f80fd1878d3321ab519ff.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    revision: '42ac5946195a7306e2a5',
+    url: '/static/js/runtime~main.a8a9905a.js'
+  },
+  {
+    revision: '65c25be9be8eba83241c',
+    url: '/static/js/main.ba38edb8.chunk.js'
+  },
+  {
+    revision: 'db0475df771227a85f17',
+    url: '/static/js/2.96260e71.chunk.js'
+  },
+  {
+    revision: 'b500615249374d11deb76f9eee7b438d',
+    url: '/index.html'
+  }
+];
